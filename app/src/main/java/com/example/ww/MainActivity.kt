827eb88868b30f1d4919f8e8fc626f6e21138608
pindrive <107409882+pindrive@users.noreply.github.com>
2023-03-25 17:59:06 +0100

    Application planning

--- app/src/main/java/com/example/ww/MainActivity.kt
+++ app/src/main/java/com/example/ww/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.ww
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Algorytm: Brute Force
+
+
+        // Algorytm KMP
+
+
+        // Algorytm BM
+
+
+        // ALGORYTM RK
+
+
+        // Pobranie danych i wypisanie wyników
+        findViewById<Button>(R.id.btn).setOnClickListener {
+
+        }
     }
 }
